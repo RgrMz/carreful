2fe294b3f399a4097f0ed4c45f9e9b386118e339
RgrMz <cargmzrub@gmail.com>
2021-04-18 13:26:10 +0200

    Creada clase abstracta Pedido y PedidoNormal

--- src/main/java/edu/uclm/esi/carreful/model/Pedido.java
+++ src/main/java/edu/uclm/esi/carreful/model/Pedido.java
@@ -1,22 +1,100 @@
 package edu.uclm.esi.carreful.model;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Inheritance
+@DiscriminatorColumn(name = "tipo")
+@DiscriminatorValue(value = "Pedido")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class Pedido {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long idPedido;
 
-	@NotNull
+	@NotNull @Column(length = 70)
 	private String nombre, apellidos, email, telefonoMovil, direccion, ciudad, provincia, pais;
 	@NotNull
 	private int codigoPostal;
-	//@NotNull
-	//private Carrito carrito;
+	@NotNull @Column(columnDefinition = "varchar(255) default 'Pendiente de envío'")
+	private String estado;
+
+	public long getIdPedido() {
+		return idPedido;
+	}
+	public void setIdPedido(long idPedido) {
+		this.idPedido = idPedido;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefonoMovil() {
+		return telefonoMovil;
+	}
+	public void setTelefonoMovil(String telefonoMovil) {
+		this.telefonoMovil = telefonoMovil;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public int getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(int codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public abstract String generarFactura(Carrito carrito);
+	public abstract void actualizarEstado();
+
 }
